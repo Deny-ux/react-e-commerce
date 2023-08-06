6d18a062262fc3c5d09f3559f9bf6959c29675d3
Deny-ux <denys.fokashchuk@gmail.com>
2023-08-06 12:22:23 +0200

    Added cart functionality

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,44 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from "../actions";
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload;
+    console.log(id);
+    const tempItem = state.cart.find((product) => product.id === id + color);
+    if (tempItem) {
+      console.log(1111111111111);
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount;
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max;
+          }
+          return { ...state, amount: newAmount };
+        } else {
+          return cartItem;
+        }
+      });
 
-export default cart_reducer
+      return { ...state, cart: tempCart };
+    } else {
+      console.log(222222222222222222);
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color: color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      };
+      return { ...state, cart: [...state.cart, newItem] };
+    }
+  }
+  return state;
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default cart_reducer;
